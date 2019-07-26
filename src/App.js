88018177dfc24b616096e3b53eab3fe6c3bb730f
Irene Fuente Romero <irene.fuente@ultimasoftware.com>
2019-07-26 13:54:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <button type="button"> Get Events</button>
+      <section>
+        <h1>holi</h1>
+      </section>
+    </div>
+  );
+};
+
+export default App;
